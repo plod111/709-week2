2b54c2b30bc6fbea25239b587e5aed31a440b074
plod111 <ben.cornish@gmail.com>
2023-09-10 11:24:58 -0400

    First commit

---
+++ week2Test.java
@@ -0,0 +1,14 @@
+
+public class week2Test {
+
+    public static void main(String[] args) {
+
+        String message = "hellloworlds!";
+
+        System.out.println(message);
+
+        System.out.print(message);
+
+    }
+
+}
